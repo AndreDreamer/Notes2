cf8df642dbe682baabef091d2bcadabdd5b63b16
Andrew Pylypiv <pylypiv10@gmail.com>
2021-09-24 14:18:26 +0300

    fixed listView update

--- app/src/main/java/com/example/notes2/MainActivity.kt
+++ app/src/main/java/com/example/notes2/MainActivity.kt
@@ -49,6 +49,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         active = true
+        listView.invalidateViews()
         removeService()
     }
 
