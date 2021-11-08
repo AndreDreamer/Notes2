c15f1fc70d64ae16a331628151c72eac0c818e0c
Andrew Pylypiv <pylypiv10@gmail.com>
2021-11-08 12:29:20 +0200

    optimized imports

--- app/src/main/java/com/example/notes2/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/notes2/MyFirebaseMessagingService.kt
@@ -6,9 +6,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
-import android.util.Log
 import androidx.core.app.NotificationCompat
-
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
